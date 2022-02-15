cf2502294fc2a0f264a3ba71d2dfaad42cb6972a
Giovane Santana <77679841+giios@users.noreply.github.com>
2022-02-15 10:58:58 -0300

    atualização 1

--- app/src/main/java/com/cursoandroid/msnapp/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/msnapp/activity/MainActivity.java
@@ -1,22 +1,23 @@
-package com.cursoandroid.msnapp;
+package com.cursoandroid.msnapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cursoandroid.msnapp.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+    //private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_validador);
 
 
-        referenciaFirebase.child("ponto").setValue(100);
+        //referenciaFirebase.child("ponto").setValue(100);
     }
 }
